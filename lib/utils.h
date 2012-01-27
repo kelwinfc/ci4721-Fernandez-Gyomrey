74a8bb9e37f7fa317a01dbb35bd51015f66fc60e
kelwinfc <kelwinfc@lisboa.(none)>
2012-01-27 00:25:16 -0430

    Avanzada gramatica. Solo falta la subgramatica de los statements. Creado archivo utils con funciones auxiliares. Modificada expresion regular para detectar comentarios en bloque, estaba incorrecta. Ya imprime toda la parte del arbol efectuada, falta comentarla. Siguientes pasos: terminar gramatica y realizar tabla de simbolos.

---
+++ lib/utils.h
@@ -0,0 +1,6 @@
+#ifndef __UTILS
+#define __UTILS
+
+void print_indentation(int k);
+
+#endif
---
+++ src/utils.cpp
@@ -0,0 +1,10 @@
+#include "utils.h"
+#include <iostream>
+
+using namespace std;
+
+void print_indentation(int k){
+    for (int i=0; i<k; i++){
+        cout << "  ";
+    }
+}
